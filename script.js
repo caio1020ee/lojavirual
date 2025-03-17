8026033b413f30c255af2e46e346287834803bff
caio1020ee <caiolucasamorim@gmail.com>
2025-03-17 09:09:37 -0300

    suba

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const products = [
         { name: "luminaria", image: "luminaria.jpg", link: "https://amzn.to/4ipfEB0" },
         { name: "fechadura", image: "fechadura.jpg", link: "https://amzn.to/4hFmaCu" },
-        { name: "Câmera", image: "camera.jpg", link: "https://amzn.to/41ICoEQ" }
+        { name: "Câmera", image: "camera.jpg", link: "https://amzn.to/41ICoEQ" },
+        { name: "cortina", image: "janela.jpg", link: "https://amzn.to/4iQCIsb" },
+        { name: "chaveiro", image: "chaveiro.jpg", link: "https://amzn.to/4iblIx4" },
+        { name: "ehance", image: "enhance.jpg", link: "https://amzn.to/4iwQjVz" },
+
+
     ];
 
     const productList = document.getElementById("product-list");
